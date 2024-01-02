081371a41e92a5dd7f14d75d68d7d479b57c2018
yankz12 <sa.may@posteo.net>
2024-01-02 18:52:54 +0100

    Structured sub-modules plotting, signal_processing and tools to src-layout

---
+++ src/audiolib/plotting/__init__.py
@@ -0,0 +1,13 @@
+from .plotting import (
+    plot_time,
+    plot_rfft_freq,
+    plot_h_full,
+    plot_rfft_ir,
+)
+
+__all__ = [
+    'plot_time',
+    'plot_rfft_freq',
+    'plot_h_full',
+    'plot_rfft_ir',
+]
